479aeb5d41284137b59ae78c18b02da5c9a662f3
Max <maxkoontz@gmail.com>
2024-01-23 14:37:30 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Max_DOCU'
+copyright = '2023, M00X'
+author = 'M00X'
 
 release = '0.1'
 version = '0.1.0'
